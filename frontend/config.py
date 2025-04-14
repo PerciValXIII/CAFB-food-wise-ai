bb6b8f4a8c0b2370f32504e9e6ccb701b3530493
Swattik Maiti <percival@Swattiks-MacBook-Pro.local>
2025-04-14 16:42:23 -0400

    Resolved merge conflict in config.py to use st.secrets

--- frontend/config.py
+++ frontend/config.py
@@ -1,4 +1,8 @@
-API_URL = ""
-PPT_GEN_URL = ""
-PDF_GEN_URL = ""
-BEARER_TOKEN = ""
+import streamlit as st
+
+# These keys must match the keys in your .streamlit/secrets.toml file.
+BEARER_TOKEN = st.secrets["general"]["BEARER_TOKEN"]
+API_URL = st.secrets["general"]["API_URL"]
+PPT_GEN_URL = st.secrets["general"]["PPT_GEN_URL"]
+PDF_GEN_URL = st.secrets["general"]["PDF_GEN_URL"]
+
