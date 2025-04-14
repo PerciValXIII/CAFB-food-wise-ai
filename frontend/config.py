95d834ebab79d4bd090804cc52e30be15e5d4da2
Swattik Maiti <50179165+PerciValXIII@users.noreply.github.com>
2025-04-14 16:00:13 -0400

    Update config.py

--- frontend/config.py
+++ frontend/config.py
@@ -1,4 +1,4 @@
-API_URL = "https://c60f-2601-14a-4100-6820-6943-6ca9-aeb4-4eae.ngrok-free.app/data/search"
-PPT_GEN_URL = "https://c60f-2601-14a-4100-6820-6943-6ca9-aeb4-4eae.ngrok-free.app/data/ppt"
-PDF_GEN_URL = "https://c60f-2601-14a-4100-6820-6943-6ca9-aeb4-4eae.ngrok-free.app/data/pdf"
-BEARER_TOKEN = "qwerty123@123"
+API_URL = ""
+PPT_GEN_URL = ""
+PDF_GEN_URL = ""
+BEARER_TOKEN = ""
